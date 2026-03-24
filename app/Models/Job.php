a2bde51fff156859bab55d025508232bd1131151
Ballesteros <b.ballesteros@honolulu.gov>
2026-03-24 10:24:01 -1000

    created Job factory and updated User factory

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,38 +1,14 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
 
-class Job {
-    public static function all():array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-    public static function find(int $id):array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+class Job extends Model{
+    use HasFactory;
 
-        if (!$job) {
-            abort(404);
-        }
+    protected $table = 'job_listings';
 
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
