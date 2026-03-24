9ae511a19859694abf0df95fa6f3dd3dc272959e
Ballesteros <b.ballesteros@honolulu.gov>
2026-03-24 08:21:27 -1000

    Job namespace class

--- routes/web.php
+++ routes/web.php
@@ -1,53 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function (){
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ]
-    ];
+Route::get('/jobs/{id}', function ($id){
 
-    $job = \Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    $job = \Illuminate\Support\Arr::first(Job::all(), fn($job) => $job['id'] == $id);
 
 
     return view('job', ['job' => $job]);
