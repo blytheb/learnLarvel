eb8c11826c5ebd98c2312adae31741a17c3b7b0e
blytheb <blytheballesteros@gmail.com>
2026-04-06 13:40:53 -1000

    Day 19 = Route Model Binding Tip

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,10 @@ Route::get('/jobs/create', function() {
 });
 
 // Show
-Route::get('/jobs/{id}', function ($id){
-
-    $job = Job::find($id);
+Route::get('/jobs/{job}', function (Job $job){
 
     return view('jobs.show', ['job' => $job]);
+
 });
 
 // Store
@@ -47,25 +46,21 @@ Route::post('/jobs', function () {
 });
 
 // Edit
-Route::get('/jobs/{id}/edit', function ($id){
-
-    $job = Job::find($id);
-
+Route::get('/jobs/{job}/edit', function (Job $job){
     return view('jobs.edit', ['job' => $job]);
 });
 
 // Update
-Route::patch('/jobs/{id}', function ($id){
+Route::patch('/jobs/{job}', function (Job $job){
+    //authorize    
+
     //validate
     request()->validate([
         'title'=>['required', 'min:3'],
         'salary'=> ['required']
     ]);
-    //authorize
 
     // update the job
-    $job = Job::findOrFail($id);
-
     $job->update([
         'title' => request('title'),
         'salary' => request('salary'),
@@ -76,11 +71,11 @@ Route::patch('/jobs/{id}', function ($id){
 });
 
 // Destroy
-Route::delete('/jobs/{id}', function ($id){
+Route::delete('/jobs/{job}', function (Job $job){
     //authorize
     
     //delete
-    Job::findOrFail($id)->delete();
+    $job->delete();
 
     //redirect
     return redirect('/jobs');
