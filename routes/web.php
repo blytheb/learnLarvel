8fa28b1af1925f3c23d06b2ce91e526eee2cb841
blytheb <blytheballesteros@gmail.com>
2026-04-06 13:33:58 -1000

    update and delete requests. full CRUD functionality

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//Index - Displays all jobs
 Route::get('/jobs', function (){
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -15,12 +16,13 @@ Route::get('/jobs', function (){
     ]);
 });
 
+// Create 
 Route::get('/jobs/create', function() {
 
     return view ('jobs.create');
 });
 
-
+// Show
 Route::get('/jobs/{id}', function ($id){
 
     $job = Job::find($id);
@@ -28,6 +30,7 @@ Route::get('/jobs/{id}', function ($id){
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title'=>['required', 'min:3'],
@@ -40,6 +43,46 @@ Route::post('/jobs', function () {
         'employer_id' => 1
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id){
+
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id){
+    //validate
+    request()->validate([
+        'title'=>['required', 'min:3'],
+        'salary'=> ['required']
+    ]);
+    //authorize
+
+    // update the job
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // redirect to job page
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id){
+    //authorize
+    
+    //delete
+    Job::findOrFail($id)->delete();
+
+    //redirect
     return redirect('/jobs');
 });
 
