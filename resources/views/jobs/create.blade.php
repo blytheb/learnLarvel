26987c2256e4714752cefa9575df3aff9e4bfa58
blytheb <blytheballesteros@gmail.com>
2026-04-06 11:13:45 -1000

    server side validation with error message

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,7 +16,7 @@
                 <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                 <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input id="title" type="text" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                    <input id="title" type="text" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
                   </div>
                 </div>
               </div>
@@ -24,12 +24,23 @@
                 <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                 <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input id="salary" type="text" name="salary" placeholder="$50,000 per year" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                    <input id="salary" type="text" name="salary" placeholder="$50,000 per year" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
                   </div>
                 </div>
               </div>
-          
             </div>
+
+            <div class="mt-10">
+              @if ($errors->any())
+              <ul>
+                @foreach ($errors->all() as $error )
+                  <li class="text-red-500 italic">{{ $error }}</li>
+                @endforeach
+              </ul>
+              @endif
+            </div>
+              
+
           </div>
       
 
