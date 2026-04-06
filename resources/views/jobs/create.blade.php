9787151308bca445eaf4a419ed01fed57d6a998b
blytheb <blytheballesteros@gmail.com>
2026-04-06 11:18:57 -1000

    client and server side validation under each input

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,20 +16,28 @@
                 <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                 <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input id="title" type="text" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
+                    <input id="title" type="text" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required \>
                   </div>
+
+                  @error('title')
+                    <p class="text-xs text-red-500 semi-bold mt-1">{{ $message}}</p>
+                  @enderror
                 </div>
               </div>
               <div class="sm:col-span-4">
                 <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                 <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input id="salary" type="text" name="salary" placeholder="$50,000 per year" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
+                    <input id="salary" type="text" name="salary" placeholder="$50,000 per year" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
                   </div>
+
+                  @error('salary')
+                    <p class="text-xs text-red-500 semi-bold mt-1">{{ $message}}</p>
+                  @enderror
                 </div>
               </div>
             </div>
-
+{{-- 
             <div class="mt-10">
               @if ($errors->any())
               <ul>
@@ -38,7 +46,7 @@
                 @endforeach
               </ul>
               @endif
-            </div>
+            </div> --}}
               
 
           </div>
